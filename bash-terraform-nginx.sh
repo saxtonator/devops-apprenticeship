c6c5fea26798596f75def596390509bcdfb0543c
saxtonator <aaron@dmsf.co.uk>
2023-08-08 22:41:15 +0100

    Update bash-terraform-nginx.sh
    Updated line 8 with additional instructions about selecting the correct Linux Distro

--- bash-terraform-nginx.sh
+++ bash-terraform-nginx.sh
@@ -5,7 +5,7 @@ mkdir learn-terraform-docker-container
 :: This working directory houses the configuration files that you write to describe the infrastructure you want Terraform to create and manage.
 :: Navigate into the working directory.
 cd learn-terraform-docker-container
-:: In the working directory, create or download the file called main.tf with the terraform configuration. You can copy this configuration from https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli 
+:: In the working directory, create or download the file called main.tf with the terraform configuration. You can copy this configuration from https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli. Make suree you select the correct version of your Linux distro.
 touch main.tf
 echo 'terraform {  required_providers {    docker = {      source  = "kreuzwerker/docker"      version = "~> 3.0.1"    }  }}provider "docker" {}resource "docker_image" "nginx" {  name         = "nginx"  keep_locally = false}resource "docker_container" "nginx" {  image = docker_image.nginx.image_id  name  = "tutorial"  ports {    internal = 80    external = 8000  }}' >> main.tf
 :: Initialize the project, which downloads a plugin called a provider that lets Terraform interact with Docker.
